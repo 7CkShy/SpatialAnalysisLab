c18911bfeaf45f35fb1a2e9fae9a167f330a4fd4
7CkShy-L <ckshy@snnu.edu.cn>
2026-03-10 10:18:10 +0800

    Changing file name and adding a LoacalMoran file

--- R/LocalG.R
+++ R/LocalG.R
@@ -16,15 +16,15 @@ nbrs
 lag_q = spatial_lag(queen_w, raw_data['Crm_prs'])
 
 # What is the spatial lag
-raw_data$Crm_prs[nbrs] |> 
+raw_data$Crm_prs[nbrs] |>
   mean()
 lag_q$Spatial.Lag[1]
 
 # Caculate the LISA
 lisa_cm = local_moran(queen_w, raw_data["Crm_prp"])
 
-cm_sf = raw_data |> 
-  select(geometry) |> 
+cm_sf = raw_data |>
+  select(geometry) |>
   bind_cols(
     data.frame("Lisa_cm" = lisa_cm$lisa_vals)
   )
@@ -42,31 +42,36 @@ splilabel = lisa_labels(localg_crmprp)
 splisac = lisa_clusters(localg_crmprp)
 splisac = factor(splilabel[splisac + 1], levels = splilabel)
 
-local_g_plot = \(sf, col_name, w_type, cpu_n, cutoff, p_on){
+local_g_plot = \(sf, col_name, w_type, cpu_n, cutoff, p_on) {
   if (w_type == "queen") {
     w = rgeoda::queen_weights(sf)
   } else if (w_type == "rook") {
     w = rgeoda::rook_weights(sf)
   }
-  var_df = sf |> 
-    sf::st_drop_geometry() |> 
-    dplyr::select(col_name)
-  tlisa = rgeoda::local_g(w, var_df, cpu_threads = cpu_n, significance_cutoff = cutoff)
+  var_df = sf |>
+    sf::st_drop_geometry() |>
+    dplyr::select(all_of(col_name))
+  tlisa = rgeoda::local_g(
+    w,
+    var_df,
+    cpu_threads = cpu_n,
+    significance_cutoff = cutoff
+  )
   lisalabel = rgeoda::lisa_labels(tlisa)
-  lisa_df = sf |> 
-    dplyr::select(col_name) |> 
+  lisa_df = sf |>
+    dplyr::select(col_name) |>
     mutate(
       lisaindex = rgeoda::lisa_clusters(tlisa),
-      lisalabindex = factor(lisalabel[lisaindex + 1], levels = lisalabel), 
+      lisalabindex = factor(lisalabel[lisaindex + 1], levels = lisalabel),
       .before = "geometry"
     )
-    return(lisa_df)
+  return(lisa_df)
 }
 
 a = local_g_plot(raw_data, "Crm_prp", "queen", 6, 0.05, TRUE)
 
 # ggplot2 practice
-a |> 
+a |>
   ggplot() +
   geom_sf(aes(fill = lisalabindex)) -> p
 
---
+++ R/LocalMoran.R
@@ -0,0 +1,52 @@
+raw_path = system.file("extdata", "Guerry.shp", package = "rgeoda")
+raw_data = sf::st_read(raw_path)
+
+local_moran_plot = \(sff, col_name, w_t, per_n, cpu_n, cut_off_n) {
+  if (w_t == "queen") {
+    w = rgeoda::queen_weights(sff)
+  } else if (w_t == "rook") {
+    w = rgeoda::rook_weights(sff)
+  }
+  ds = sff |>
+    sf::st_drop_geometry() |>
+    dplyr::select(all_of(col_name))
+  lisa = rgeoda::local_moran(
+    w,
+    ds,
+    permutations = per_n,
+    cpu_threads = cpu_n,
+    significance_cutoff = cut_off_n
+  )
+  lisaLabes = rgeoda::lisa_labels(lisa)
+  lisa_sf_df = sff |>
+    dplyr::select(col_name) |>
+    dplyr::mutate(
+      lisaIndex = rgeoda::lisa_clusters(lisa),
+      lisaLabIndex = factor(lisaLabes[lisaIndex + 1], levels = lisaLabes),
+      .before = "geometry"
+    )
+  return(lisa_sf_df)
+}
+
+local_moran_plot(raw_data, "Crm_prp", "queen", 999, 8, 0.05) -> moran_df
+
+# ggplot plot
+library(ggplot2)
+
+moran_df |>
+  ggplot() +
+  geom_sf(aes(fill = lisaLabIndex)) -> p_m
+
+p_m +
+  scale_fill_manual(
+    values = c(
+      "Not significant" = "#f2f2f2",
+      "High-High" = "red",
+      "Low-Low" = "blue",
+      "Low-High" = "green"
+    )
+  ) +
+  guides(
+    fill = guide_legend(title = "LISA")
+  ) +
+  theme_bw()
